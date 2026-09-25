4ee6c13b4c976554d936d57ab48cd7ae6765f96a
Aditya Vijay kumar <avkad3@gmail.com>
2026-09-25 13:39:24 -0700

    Fix Ruff import formatting

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
+import joblib
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
-import pandas as pd
-import joblib
 
 model = joblib.load("tuned_random_forest.pkl")
 
